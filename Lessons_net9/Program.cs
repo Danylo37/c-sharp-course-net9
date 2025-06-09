d7f2505ff37e91f7e002952f0e2bfcd5c9852c17
Danylo Lopatin <d.s.lopatin@student.khai.edu>
2025-06-09 22:09:40 +0200

    Doc string and overloading

--- Lessons_net9/Program.cs
+++ Lessons_net9/Program.cs
@@ -2,37 +2,32 @@
 
 class Program
 {
+    /// <summary>
+    /// Returns the sum of two integers 
+    /// </summary>
+    /// <param name="a">first value</param>
+    /// <param name="b">second value</param>
+    /// <returns>sum of a and b</returns>
+    static int Sum(int a, int b)
+    {
+        return a + b;
+    }
+
+    /// <summary>
+    /// Returns the sum of three integers  
+    /// </summary>
+    /// <param name="a">first value</param>
+    /// <param name="b">second value</param>
+    /// <param name="c">third value</param>
+    /// <returns>sum of a, b and c</returns>
+    static int Sum(int a, int b, int c)
+    {
+        return a + b + c;
+    }
+    
     static void Main(string[] args)
     {
-        int[,] matrix =
-        {
-            {132, 24, 3},
-            {4, 532, 63},
-            {721, 82, 912}
-        };
-
-        foreach (var item in matrix)
-        {
-            Console.Write(item + " ");
-        }
-
-        Console.WriteLine();
-        Console.WriteLine();
-
-        Console.WriteLine(" \t0\t1\t2");
-        for (int row = 0; row < matrix.GetLength(0); row++)
-        {
-            Console.Write($"{row}\t");
-            for (int col = 0; col < matrix.GetLength(1); col++)
-            {
-                Console.Write(matrix[row, col] + "\t");
-            }
-            Console.WriteLine();
-        }
-
-        Console.WriteLine();
-
-        Console.WriteLine("Dimensions: " + matrix.Rank);
-        Console.WriteLine("Length: " + matrix.Length);
+        Console.WriteLine(Sum(10, 20));
+        Console.WriteLine(Sum(10, 20, 30));
     }
 }
