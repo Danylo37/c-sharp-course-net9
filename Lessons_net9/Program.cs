f12cf8d7552e77e12da32bb707035978c3364cbf
Danylo Lopatin <d.s.lopatin@student.khai.edu>
2025-06-09 22:27:52 +0200

    '?', '??' and '??=' operators

--- Lessons_net9/Program.cs
+++ Lessons_net9/Program.cs
@@ -2,32 +2,24 @@
 
 class Program
 {
-    /// <summary>
-    /// Returns the sum of two integers 
-    /// </summary>
-    /// <param name="a">first value</param>
-    /// <param name="b">second value</param>
-    /// <returns>sum of a and b</returns>
-    static int Sum(int a, int b)
-    {
-        return a + b;
-    }
-
-    /// <summary>
-    /// Returns the sum of three integers  
-    /// </summary>
-    /// <param name="a">first value</param>
-    /// <param name="b">second value</param>
-    /// <param name="c">third value</param>
-    /// <returns>sum of a, b and c</returns>
-    static int Sum(int a, int b, int c)
-    {
-        return a + b + c;
-    }
-    
     static void Main(string[] args)
     {
-        Console.WriteLine(Sum(10, 20));
-        Console.WriteLine(Sum(10, 20, 30));
+        string str = null;
+        
+        string result = str ?? "Null value";
+        
+        Console.WriteLine($"str is: {str}");
+        Console.WriteLine($"Result is: {result}");
+
+        string str2 = null;
+        // string str2 = "Not null value";
+
+        str2 ??= "Null value";
+        
+        Console.WriteLine(str2);
+
+        int[] array = null;
+
+        Console.WriteLine("Sum: " + (array?.Sum() ?? 0));
     }
 }
