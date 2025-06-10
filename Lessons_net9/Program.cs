08ed4e4a89eefcbac1e5626d1cb141f56a1d60ea
Danylo Lopatin <d.s.lopatin@student.khai.edu>
2025-06-10 13:32:40 +0200

    out

--- Lessons_net9/Program.cs
+++ Lessons_net9/Program.cs
@@ -2,28 +2,12 @@
 
 class Program
 {
-    static void Resize(ref int[] arr, int newLength)
-    {
-        int[] newArr = new int[newLength];
-        
-        int itemsToAdd = newLength < arr.Length ? newLength : arr.Length;
-        
-        for (int i = 0; i < itemsToAdd; i++)
-        {
-            newArr[i] = arr[i];
-        }
-        
-        arr = newArr;
-    }
-    
     static void Main(string[] args)
     {
-        int[] arr = { 6, 23, 534, 22, 76, 12, 55, 123, 37, 88 };
-        Resize(ref arr, 6);
-
-        foreach (var item in arr)
-        {
-            Console.Write(item + " ");
-        }
+        string input = Console.ReadLine();
+        
+        int.TryParse(input, out int number);
+        
+        Console.WriteLine(number);
     }
 }
