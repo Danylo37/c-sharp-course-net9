c7f233b2fd7d559d78774fba74391740cc08f59e
Danylo Lopatin <d.s.lopatin@student.khai.edu>
2025-06-10 13:53:54 +0200

    in

--- Lessons_net9/Program.cs
+++ Lessons_net9/Program.cs
@@ -1,13 +1,51 @@
-﻿namespace Lessons_net9;
+﻿using System.Diagnostics;
+
+namespace Lessons_net9;
 
 class Program
 {
+    struct MyStruct
+    {
+        public decimal a;
+        public decimal b;
+        public decimal c;
+        public decimal d;
+        public decimal e;
+        public decimal f;
+    }
+
+    static void Foo(MyStruct value)
+    {
+        
+    }
+
+    static void Bar(in MyStruct value)
+    {
+        // value.a = 5;     // error, value is read-only
+    }
+    
     static void Main(string[] args)
     {
-        string input = Console.ReadLine();
+        MyStruct a = new MyStruct();
+
+        Stopwatch sw = Stopwatch.StartNew();
         
-        int.TryParse(input, out int number);
+        for (int i = 0; i < int.MaxValue; i++)
+        {
+            Foo(a);
+        }
         
-        Console.WriteLine(number);
+        sw.Stop();
+        Console.WriteLine("Foo: " + sw.ElapsedMilliseconds + " milliseconds");  // Foo: 6199 milliseconds
+        
+        sw.Restart();
+        
+        for (int i = 0; i < int.MaxValue; i++)
+        {
+            Bar(a);
+        }
+        
+        sw.Stop();
+        Console.WriteLine("Bar: " + sw.ElapsedMilliseconds + " milliseconds");  // Bar: 3817 milliseconds
     }
 }
