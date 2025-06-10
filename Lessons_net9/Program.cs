e95c24f4611f3032d2c7179490c496204c5954fc
Danylo Lopatin <d.s.lopatin@student.khai.edu>
2025-06-10 12:28:36 +0200

    ref

--- Lessons_net9/Program.cs
+++ Lessons_net9/Program.cs
@@ -2,24 +2,28 @@
 
 class Program
 {
+    static void Resize(ref int[] arr, int newLength)
+    {
+        int[] newArr = new int[newLength];
+        
+        int itemsToAdd = newLength < arr.Length ? newLength : arr.Length;
+        
+        for (int i = 0; i < itemsToAdd; i++)
+        {
+            newArr[i] = arr[i];
+        }
+        
+        arr = newArr;
+    }
+    
     static void Main(string[] args)
     {
-        string str = null;
-        
-        string result = str ?? "Null value";
-        
-        Console.WriteLine($"str is: {str}");
-        Console.WriteLine($"Result is: {result}");
+        int[] arr = { 6, 23, 534, 22, 76, 12, 55, 123, 37, 88 };
+        Resize(ref arr, 6);
 
-        string str2 = null;
-        // string str2 = "Not null value";
-
-        str2 ??= "Null value";
-        
-        Console.WriteLine(str2);
-
-        int[] array = null;
-
-        Console.WriteLine("Sum: " + (array?.Sum() ?? 0));
+        foreach (var item in arr)
+        {
+            Console.Write(item + " ");
+        }
     }
 }
