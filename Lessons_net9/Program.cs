8ebd6bdca74664cf4c6cadb8fc30db11ddecbe03
Danylo Lopatin <d.s.lopatin@student.khai.edu>
2025-06-10 14:12:40 +0200

    default params

--- Lessons_net9/Program.cs
+++ Lessons_net9/Program.cs
@@ -15,11 +15,23 @@ class Program
 
         return sum;
     }
+
+    static void Test(int a, int b = 2, int c = 5)
+    {
+        Console.WriteLine(Sum(a, b, c));
+    }
     
     static void Main(string[] args)
     {
         Console.WriteLine(Sum(3, 7));
         Console.WriteLine(Sum(5, 7, 3));
         Console.WriteLine(Sum(5, 7, 12, 11));
+        
+        Console.WriteLine();
+        
+        Test(1, 3, 6);
+        Test(1, 3);
+        Test(1);
+        Test(1, c:4);
     }
 }
