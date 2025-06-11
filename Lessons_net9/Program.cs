dd26197dffc23aff8c3cc34f68c0f452d0bd093d
Danylo Lopatin <d.s.lopatin@student.khai.edu>
2025-06-11 11:23:36 +0200

    enums

--- Lessons_net9/Program.cs
+++ Lessons_net9/Program.cs
@@ -2,25 +2,62 @@
 
 class Program
 {
+    enum DayOfWeek : byte
+    {
+        Monday = 1,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+    }
+
+    static DayOfWeek GetNextDay(DayOfWeek day)
+    {
+        if (day < DayOfWeek.Sunday)
+        {
+            return day + 1;
+        }
+
+        return DayOfWeek.Monday;
+    }
+
     static void Main(string[] args)
     {
-        while (true)
+        var mon = DayOfWeek.Monday;
+
+        Console.WriteLine(mon);
+        Console.WriteLine((DayOfWeek)1);
+        Console.WriteLine((int)mon);
+        Console.WriteLine(mon.GetType());
+        Console.WriteLine(Enum.GetUnderlyingType(typeof(DayOfWeek)));
+
+        Console.WriteLine(GetNextDay(mon));
+
+        DayOfWeek day;
+        byte value = 55;
+
+        // if (Enum.IsDefined(typeof(DayOfWeek), value))
+        // {
+        //     day = (DayOfWeek)value;
+        // }
+        // else
+        // {
+        //     throw new Exception("Invalid DayOfWeek value");
+        // }
+
+        var values = Enum.GetValues(typeof(DayOfWeek));
+
+        Console.WriteLine();
+
+        foreach (DayOfWeek dayOfWeek in values)
         {
-            ConsoleKey key = Console.ReadKey().Key;
-            
-            int keyCode = (int)key;
-            
-            Console.WriteLine($"\tEnum: {key}\tKey Code: {keyCode}");
-
-            if (key == ConsoleKey.Enter)
-            {
-                Console.WriteLine("You pressed Enter.");
-            }
-
-            if (key == ConsoleKey.Q)
-            {
-                break;
-            }
+            Console.WriteLine(dayOfWeek);
         }
+
+        string str = Console.ReadLine();
+        DayOfWeek d = (DayOfWeek)Enum.Parse(typeof(DayOfWeek), str, ignoreCase: true);
+        Console.WriteLine(d);
     }
 }
