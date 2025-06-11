5d2ca2127395c2563d2f4f861e973bd7c9b17f6a
Danylo Lopatin <d.s.lopatin@student.khai.edu>
2025-06-11 10:56:42 +0200

    var

--- Lessons_net9/Program.cs
+++ Lessons_net9/Program.cs
@@ -2,28 +2,29 @@
 
 class Program
 {
+    // class Test
+    // {
+    //     public var a;    // error
+    // }
+
+    // static var Foo()     // error
+    // {
+    //
+    // }
+
+    // static void Foo(var a)   // error
+    // {
+    //
+    // }
+
     static void Main(string[] args)
     {
-        Nullable<int> a = null;
+        var dict = new Dictionary<int, string>();
+        var anonType = new { Name = "name", Age = 0 };
+        var result = from i in new[] { 1, 2, 3, 4, 5 } where i % 2 == 0 orderby i select i;
         
-        int? b = null;
-        
-        Console.WriteLine(a);
-        
-        Console.WriteLine(a == null);
-        
-        Console.WriteLine(a.HasValue);
-        
-        Console.WriteLine(a.GetValueOrDefault());
-        
-        Console.WriteLine(a.GetValueOrDefault(5));
-        
-        Console.WriteLine(a ?? 10);
-        
-        // Console.WriteLine(i.Value);     // InvalidOperationException
-        
-        Console.WriteLine((a + 5).HasValue);
-        
-        Console.WriteLine(a == 5);
+        Console.WriteLine(dict.GetType());
+        Console.WriteLine(anonType.GetType());
+        Console.WriteLine(result.GetType());
     }
 }
