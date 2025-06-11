91c6d28c9b9ca4425ff8c7bb6092b7bb9cb19d5f
Danylo Lopatin <d.s.lopatin@student.khai.edu>
2025-06-11 11:03:21 +0200

    ConsoleKey enum

--- Lessons_net9/Program.cs
+++ Lessons_net9/Program.cs
@@ -2,29 +2,25 @@
 
 class Program
 {
-    // class Test
-    // {
-    //     public var a;    // error
-    // }
-
-    // static var Foo()     // error
-    // {
-    //
-    // }
-
-    // static void Foo(var a)   // error
-    // {
-    //
-    // }
-
     static void Main(string[] args)
     {
-        var dict = new Dictionary<int, string>();
-        var anonType = new { Name = "name", Age = 0 };
-        var result = from i in new[] { 1, 2, 3, 4, 5 } where i % 2 == 0 orderby i select i;
-        
-        Console.WriteLine(dict.GetType());
-        Console.WriteLine(anonType.GetType());
-        Console.WriteLine(result.GetType());
+        while (true)
+        {
+            ConsoleKey key = Console.ReadKey().Key;
+            
+            int keyCode = (int)key;
+            
+            Console.WriteLine($"\tEnum: {key}\tKey Code: {keyCode}");
+
+            if (key == ConsoleKey.Enter)
+            {
+                Console.WriteLine("You pressed Enter.");
+            }
+
+            if (key == ConsoleKey.Q)
+            {
+                break;
+            }
+        }
     }
 }
