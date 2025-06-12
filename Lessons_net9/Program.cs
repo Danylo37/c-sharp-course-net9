1c64186d375c6436d8a0001375980ea260e73c6d
Danylo Lopatin <d.s.lopatin@student.khai.edu>
2025-06-12 09:24:22 +0200

    inheritance

--- Lessons_net9/Program.cs
+++ Lessons_net9/Program.cs
@@ -1,29 +1,46 @@
 ﻿namespace Lessons_net9;
 
+class Person
+{
+    public Person()
+    {
+        Console.WriteLine("Person constructor");
+    }
+
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+
+    public void PrintName()
+    {
+        Console.WriteLine(FirstName);
+    }
+}
+
+class Student : Person
+{
+    public Student()
+    {
+        Console.WriteLine("Student constructor");
+    }
+
+    public void PrintFullName()
+    {
+        Console.WriteLine(FirstName + " " + LastName);
+    }
+}
+
+
 class Program
 {
     static void Main(string[] args)
     {
-        var t = new Test(test: "test");
+        var st = new Student
+        {
+            FirstName = "John",
+            LastName = "Doe"
+        };
 
-        t.PrintTest();
-    }
-}
-
-partial class Test
-{
-    public string test;
-
-    public Test(string test)
-    {
-        this.test = test;
-    }
-}
-
-partial class Test
-{
-    public void PrintTest()
-    {
-        Console.WriteLine(test);
+        st.PrintName();
+        st.PrintFullName();
     }
 }
