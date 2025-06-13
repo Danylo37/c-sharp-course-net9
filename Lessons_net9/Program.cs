26f8124951345613bd8e14d9efce7328035f2d95
Danylo Lopatin <d.s.lopatin@student.khai.edu>
2025-06-13 13:57:15 +0200

    events

--- Lessons_net9/Program.cs
+++ Lessons_net9/Program.cs
@@ -1,16 +1,51 @@
 ﻿namespace Lessons_net9;
 
+public delegate void Notify(string msg);
+
+class MessagePublisher
+{
+    public event Notify OnNotify;
+
+    public void RaiseEvent(string msg)
+    {
+        OnNotify?.Invoke(msg);
+    }
+}
+
+class SmsSubscriber
+{
+    public void ReceiveSms(string msg)
+    {
+        Console.WriteLine("Sms received: " + msg);
+    }
+}
+
+class EmailSubscriber
+{
+    public void ReceiveEmail(string msg)
+    {
+        Console.WriteLine("Email received: " + msg);
+    }
+}
+
 internal static class Program 
 {
     private static void Main()
     {
-        var numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-        
-        var result = numbers.Where(num => num % 2 == 0);
+        MessagePublisher messagePublisher = new MessagePublisher();
 
-        foreach (var number in result)
-        {
-            Console.WriteLine(number);
-        }
+        var smsReceiver = new SmsSubscriber();
+        var emailReceiver = new EmailSubscriber();
+
+        messagePublisher.OnNotify += smsReceiver.ReceiveSms;
+        messagePublisher.OnNotify += emailReceiver.ReceiveEmail;
+
+        messagePublisher.RaiseEvent("Hello World!");
+        
+        Console.WriteLine();
+
+        messagePublisher.OnNotify -= smsReceiver.ReceiveSms;
+
+        messagePublisher.RaiseEvent("Hello World!");
     }
 }
