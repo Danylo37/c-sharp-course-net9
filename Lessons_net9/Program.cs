d6490f8c5741ce3571ce8c8cec3ee4cbda00225e
Danylo Lopatin <d.s.lopatin@student.khai.edu>
2025-06-13 08:07:26 +0200

    boxing / unboxing

--- Lessons_net9/Program.cs
+++ Lessons_net9/Program.cs
@@ -1,14 +1,35 @@
 ﻿namespace Lessons_net9;
 
+interface IPrintable
+{
+    void Print();
+}
+
+struct Point : IPrintable
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+    
+    public void Print()
+    {
+        Console.WriteLine($"X: {X}\tY: {Y}");
+    }
+}
+
 internal static class Program
 {
+    private static void Test(IPrintable printable)
+    {
+        printable.Print();
+    }
+    
     private static void Main()
     {
-        ILogger consoleLogger = new ConsoleLogger();
-
-        consoleLogger.Log(LogLevel.Debug, "some event");
-        consoleLogger.Log(LogLevel.Warning, "some warning");
-        consoleLogger.Log(LogLevel.Fatal, "some fatal error");
-        consoleLogger.LogError("some error");
+        int a = 1;
+        a.GetType();        // boxing
+        object b = a;       // boxing
+        int c = (int)b;     // unboxing
+        
+        Test(new Point());  // boxing
     }
 }
