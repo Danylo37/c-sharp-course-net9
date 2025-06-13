1eb7ba93487bf4b59a26819d5a6d24f227818566
Danylo Lopatin <d.s.lopatin@student.khai.edu>
2025-06-13 11:57:15 +0200

    IEnumerable and IEnumerator

---
+++ Lessons_net9/MyList.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+
+namespace Lessons_net9;
+
+public class MyList<T> : IEnumerable<T>
+{
+    private T[] _array = [];
+    
+    public int Count => _array.Length;
+
+    public void Add(T item)
+    {
+        var newArray = new T[_array.Length + 1];
+
+        for (int i = 0; i < _array.Length; i++)
+        {
+            newArray[i] = _array[i];
+        }
+        newArray[_array.Length] = item;
+        
+        _array = newArray;
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        return new MyEnumerator<T>(0, Count, _array);
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
--- Lessons_net9/Program.cs
+++ Lessons_net9/Program.cs
@@ -1,65 +1,18 @@
 ﻿namespace Lessons_net9;
 
-class MyList<T>
-{
-    private T[] _array = Array.Empty<T>();
-
-    public T this[int index]
-    {
-        get => _array[index];
-        set => _array[index] = value;
-    }
-    
-    public int Count => _array.Length;
-
-    public void Add(T value)
-    {
-        var newArray = new T[_array.Length + 1];
-
-        for (int i = 0; i < _array.Length; i++)
-        {
-            newArray[i] = _array[i];
-        }
-        newArray[_array.Length] = value;
-        
-        _array = newArray;
-    }
-}
-
 internal static class Program 
 {
     private static void Main()
     {
-        int a = 1, b = 5;
-        Console.WriteLine($"a = {a}\tb = {b}");
-        Swap(ref a, ref b);
-        Console.WriteLine($"a = {a}\tb = {b}\n");
+        MyList<int> list = new MyList<int>();
+        list.Add(1);
+        list.Add(2);
+        list.Add(3);
+        list.Add(4);
 
-
-        string str1 = "Hello", str2 = "World";
-        Console.WriteLine($"str1 = {str1}\tstr2 = {str2}");
-        Swap(ref str1, ref str2);
-        Console.WriteLine($"str1 = {str1}\tstr2 = {str2}\n");
-
-
-        int result = Foo<int>();
-        Console.WriteLine($"result = {result}\n");
-        
-        
-        MyList<int> myList = new MyList<int>();
-        myList.Add(1);
-        myList.Add(2);
-        Console.WriteLine(myList.Count);
-        Console.WriteLine(myList[1]);
-    }
-
-    private static void Swap<T>(ref T a, ref T b)
-    {
-        (a, b) = (b, a);
-    }
-
-    private static T Foo<T>()
-    {
-        return default(T);
+        foreach (var item in list)
+        {
+            Console.Write(item + " ");
+        }
     }
 }
