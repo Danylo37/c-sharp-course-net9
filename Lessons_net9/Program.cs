3624fe9d3b3104c429d21340a4d6c492ab808e07
Danylo Lopatin <d.s.lopatin@student.khai.edu>
2025-06-13 10:52:34 +0200

    generics

--- Lessons_net9/Program.cs
+++ Lessons_net9/Program.cs
@@ -1,35 +1,65 @@
 ﻿namespace Lessons_net9;
 
-interface IPrintable
+class MyList<T>
 {
-    void Print();
-}
+    private T[] _array = Array.Empty<T>();
 
-struct Point : IPrintable
-{
-    public int X { get; set; }
-    public int Y { get; set; }
-    
-    public void Print()
+    public T this[int index]
     {
-        Console.WriteLine($"X: {X}\tY: {Y}");
+        get => _array[index];
+        set => _array[index] = value;
+    }
+    
+    public int Count => _array.Length;
+
+    public void Add(T value)
+    {
+        var newArray = new T[_array.Length + 1];
+
+        for (int i = 0; i < _array.Length; i++)
+        {
+            newArray[i] = _array[i];
+        }
+        newArray[_array.Length] = value;
+        
+        _array = newArray;
     }
 }
 
-internal static class Program
+internal static class Program 
 {
-    private static void Test(IPrintable printable)
-    {
-        printable.Print();
-    }
-    
     private static void Main()
     {
-        int a = 1;
-        a.GetType();        // boxing
-        object b = a;       // boxing
-        int c = (int)b;     // unboxing
+        int a = 1, b = 5;
+        Console.WriteLine($"a = {a}\tb = {b}");
+        Swap(ref a, ref b);
+        Console.WriteLine($"a = {a}\tb = {b}\n");
+
+
+        string str1 = "Hello", str2 = "World";
+        Console.WriteLine($"str1 = {str1}\tstr2 = {str2}");
+        Swap(ref str1, ref str2);
+        Console.WriteLine($"str1 = {str1}\tstr2 = {str2}\n");
+
+
+        int result = Foo<int>();
+        Console.WriteLine($"result = {result}\n");
         
-        Test(new Point());  // boxing
+        
+        MyList<int> myList = new MyList<int>();
+        myList.Add(1);
+        myList.Add(2);
+        Console.WriteLine(myList.Count);
+        Console.WriteLine(myList[1]);
+    }
+
+    private static void Swap<T>(ref T a, ref T b)
+    {
+        (a, b) = (b, a);
+    }
+
+    private static T Foo<T>()
+    {
+        return default(T);
     }
 }
